8f830b3714625cc7aabce180aa4198227e14010c
ringnyu <ringnyum@gmail.com>
2022-06-23 16:17:26 +0100

    adding files to my repositoty

---
+++ index.js
@@ -0,0 +1,14 @@
+const  myName = "Moba Melvis Ringnyu";
+let height = "185cm";
+let country = "Cameroon";
+
+alert(
+  "My name is " +
+    myName +
+    "\n" +
+    "My Height is " +
+    height +
+    "\n" +
+    "I am from " +
+    country
+);
